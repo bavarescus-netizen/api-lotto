931bbdc093b8a208bb9fe770ad60e65d83dd23d9
bavarescus-netizen <bavarescus@gmail.com>
2026-02-09 22:48:34 -0400

    Add new router for entrenar and include in app

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from db import get_db
 from app.routes.prediccion import router as prediccion_router
 from app.routes.historico import router as historico_router
 from app.routes.metricas import router as metricas_router  # 👈 NUEVO
+from app.routes.entrenar import router as entrenar_router
 
 app = FastAPI(title="API Lotto Activo 🚀")
 
 app.include_router(prediccion_router)
 app.include_router(historico_router)
 app.include_router(metricas_router)  # 👈 NUEVO
+app.include_router(entrenar_router)
 
 
 @app.get("/")
