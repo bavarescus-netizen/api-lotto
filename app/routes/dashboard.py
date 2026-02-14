a0a41874c48e3dd7b5d93f73b6029f6ded17acf1
bavarescus-netizen <bavarescus@gmail.com>
2026-02-14 19:29:21 -0400

    Update dashboard.py

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -5,7 +5,10 @@ from pathlib import Path
 
 router = APIRouter()
 
-BASE_DIR = Path(__file__).resolve().parents[1]
+# .parents[0] es 'routes'
+# .parents[1] es 'app'
+# .parents[2] es la raíz del proyecto (donde está 'templates')
+BASE_DIR = Path(__file__).resolve().parents[2]
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @router.get("/dashboard", response_class=HTMLResponse)
